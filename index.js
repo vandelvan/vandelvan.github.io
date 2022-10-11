0bdf18acdd85266e56164eda4b380bec8c951ecd
vandelvan <vandel_van@hotmail.com>
2022-10-11 18:47:48 -0500

    Translation + styles

--- index.js
+++ index.js
@@ -1,1 +1,176 @@
-//TO-DO multilingual version
+texts = [
+  "skillstxt",
+  "experiencetxt",
+  "projectstxt",
+  "title",
+  "intro",
+  "knowme",
+  "contactbtn",
+  "skillstitle",
+  "skillssub",
+  "skillsdesc",
+  "experiencetitle",
+  "experiencesub",
+  "experiencedesc",
+  "ramendesc",
+  "twindesc",
+  "sunaedesc",
+  "netwardesc",
+  "educationtitle",
+  "educationsub",
+  "educationdesc",
+  "cuceisub",
+  "cuceidesc",
+  "polisub",
+  "polidesc",
+  "projectstitle",
+  "projectssub",
+  "projectsdesc",
+  "contactfooter",
+];
+json = {};
+en = {
+  skillstxt: "Skills",
+  experiencetxt: "Experience",
+  projectstxt: "Projects",
+  title: "Junior Developer, Student",
+  intro:
+    "I'm a Mexican 🇲🇽 23-year-old junior full-stack software developer and a computer engineering student with 3+ years of professional experience in web and mobile development.",
+  knowme: "Get to know me! 👇",
+  contactbtn: "Contact me",
+  skillstitle: "Skills",
+  skillssub: "Technologies & Tools",
+  skillsdesc:
+    "During my career, I've used multiple tools, languages, and frameworks for different purposes, such as web, mobile, desktop, and videogame development, automating processes, or administrating systems, such as:",
+  experiencetitle: "Experience",
+  experiencesub: "Get to know where I've worked.",
+  experiencedesc:
+    "I've been part of some different big and small teams where I've learned a lot of new things.",
+  ramendesc:
+    "I started a team of students and freelancers to create miscellaneous solutions for different clients around the world and to help them kickstart their projects and ideas.",
+  twindesc:
+    "I developed e-commerce solutions for clients of different fields using WordPress and Woocommerce, I also took part in the development of Android and iOS Apps using Flutter.",
+  sunaedesc:
+    "SUNAE was an association formed by students all around México to help high school seniors and college juniors with different careers most basic skills such as coding and math for engineering students.",
+  netwardesc:
+    "Worked on various cloud-based solutions for different businesses such as a POS (Appministra), an accounting solution (Acontia), a restaurant solution (Foodware), and a logistics solution (Trooxer).",
+  educationtitle: "Education",
+  educationsub: "Get to know where I've studied.",
+  educationdesc:
+    "I've been studying computer programming for 7+ years, I'm currently an active student always striving to learn more.",
+  cuceisub: "Bachelor in computer engineering",
+  cuceidesc: "Currently in 7th semester out of 8.",
+  polisub: "Professional Technologist in Computer Systems",
+  polidesc: "Graduated with excellence.",
+  projectstitle: "Projects",
+  projectssub: "Get to know what projects I've worked in.",
+  projectsdesc:
+    "Some of the projects I've worked in, some of them by myself, others in teams, but all of them were done with determination.",
+  contactfooter: "Contact me",
+};
+es = {
+  skillstxt: "Habilidades",
+  experiencetxt: "Experiencia",
+  projectstxt: "Proyectos",
+  title: "Desarrollador Junior, Estudiante",
+  intro:
+    "Soy un desarrollador de software full-stack, estudiante de ingeniería en computación Mexicano 🇲🇽 de 23 años, con +3 años de experiencia profesional en desarrollo web y móvil",
+  knowme: "¡Conóceme! 👇",
+  contactbtn: "Contactame",
+  skillstitle: "Habilidades",
+  skillssub: "Tecnologías y herramientas",
+  skillsdesc:
+    "Durante mi carrera, he usado múltiples herramientas, lenguajes, y frameworks para diferentes propósitos, como desarrollo web, móvil, de escritorio y de videojuegos, automatización de procesos o administración de sistemas, como:",
+  experiencetitle: "Experiencia",
+  experiencesub: "Conoce dónde he trabajado",
+  experiencedesc:
+    "He sido parte de distintos equipos chicos y grandes, donde he aprendido muchas cosas.",
+  ramendesc:
+    "Comencé un equipo de estudiantes y freelancers para crear soluciones diversas para diferentes clientes al rededor del mundo y ayudarles a iniciar sus proyectos e ideas",
+  twindesc:
+    "Desarrollé soluciones e-commerce para clientes de distintos campos usando WordPress y Woocommerce, también desarrollé aplicaciones de Android e iOS usando Flutter",
+  sunaedesc:
+    "SUNAE era una asociación formada por estudiantes de todo México para ayudar a estudiantes de los ultimos grados de preparatoria y de primeros grados de universidad con las habilidades mas basicas de diferetntes carreras como programación y matemáticas para estudiantes de ingeniería",
+  netwardesc:
+    "Trabajé en varias soluciones en la nube para distintos negocios como un punto de venta (Appministra), una solución de contabilidad (Acontia), una solución para restaurantes (Foodware), y una solución de logistica (Trooxer).",
+  educationtitle: "Educación",
+  educationsub: "Conocé donde he estudiado.",
+  educationdesc:
+    "He estudiado programación por +7 años, actualmente soy un estudiante activo siempre esperando aprender más.",
+  cuceisub: "Licenciatura en Ingeniería en Computación",
+  cuceidesc: "Actualmente en 7mo semestre de 8.",
+  polisub: "Tecnólogo Profesional en Sistemas Informáticos",
+  polidesc: "Titulado por excelencia",
+  projectstitle: "Proyectos",
+  projectssub: "Conoce los proyectos en los que he trabajado.",
+  projectsdesc:
+    "Algunos de los proyectos en los que he trabajado, algunos por mí mismo, otros en equipo, pero todos hechos con determinación.",
+  contactfooter: "Contactame",
+};
+fr = {
+  skillstxt: "Compétences",
+  experiencetxt: "Expérience",
+  projectstxt: "Projets",
+  title: "Développeur Junior, Étudiant",
+  intro:
+    "Je suis un Mexicain 🇲🇽 de 23 ans, développeur full-stack junior de software et un étudiant en génie informatique avec plus de 3 ans d'expérience professionnelle dans le développement web et mobile.",
+  knowme: "Connais moi! 👇",
+  contactbtn: "Contactez moi",
+  skillstitle: "Compétences",
+  skillssub: "Technologies et outils",
+  skillsdesc:
+    "Au cours de ma carrière, j'ai utilisé plusieurs outils, langages et frameworks à des fins différentes, telles que le développement Web, mobile, de bureau et de jeux vidéo, l'automatisation de processus ou l'administration de systèmes, tels que:",
+  experiencetitle: "Expérience",
+  experiencesub: "Découvrez où j'ai travaillé.",
+  experiencedesc:
+    "J'ai fait partie de différentes grandes et petites équipes où j'ai appris beaucoup de nouvelles choses.",
+  ramendesc:
+    "J'ai créé une équipe d'étudiants et de pigistes pour créer diverses solutions pour différents clients à travers le monde et pour les aider à lancer leurs projets et leurs idées.",
+  twindesc:
+    "J'ai développé des solutions de commerce électronique pour des clients de différents domaines en utilisant WordPress et Woocommerce, j'ai également participé au développement d'applications Android et iOS en utilisant Flutter.",
+  sunaedesc:
+    "SUNAE était une association formée par des étudiants de tout le Mexique pour aider les lycéens et les juniors avec différentes carrières, les compétences les plus élémentaires telles que le codage et les mathématiques pour les étudiants en génie.",
+  netwardesc:
+    "A travaillé sur diverses solutions basées sur le cloud pour différentes entreprises telles qu'un point de vente (Appministra), une solution de comptabilité (Acontia), une solution de restaurant (Foodware) et une solution logistique (Trooxer).",
+  educationtitle: "Éducation",
+  educationsub: "Savoir où j'ai étudié",
+  educationdesc:
+    "J'étudie la programmation informatique depuis plus de 7 ans, je suis actuellement un étudiant actif qui cherche toujours à en savoir plus.",
+  cuceisub: "Baccalauréat en génie informatique",
+  cuceidesc: "Actuellement en 7ème semestre sur 8.",
+  polisub: "Technologue professionnel en systèmes informatiques",
+  polidesc: "Diplômé avec excellence.",
+  projectstitle: "Projets",
+  projectssub: "Découvrez les projets sur lesquels j'ai travaillé.",
+  projectsdesc:
+    "Certains des projets sur lesquels j'ai travaillé, certains seuls, d'autres en équipe, mais tous ont été réalisés avec détermination. Certains des projets sur lesquels j'ai travaillé, certains seuls, d'autres en équipes, mais tous ont été faits avec détermination.",
+  contactfooter: "Contactez moi",
+};
+function translate() {
+  texts.forEach((t) => {
+    document.getElementById(t).innerHTML = json[t];
+  });
+}
+function spanish() {
+  json = es;
+  translate();
+}
+function english() {
+  json = en;
+  translate();
+}
+function french() {
+  json = fr;
+  translate();
+}
+
+function openmenu() {
+  document.getElementById("menusm").style.visibility = "visible";
+}
+
+function closemenu() {
+  document.getElementById("menusm").style.visibility = "hidden";
+}
+
+english();
+closemenu();
